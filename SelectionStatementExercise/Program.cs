a2dc33471ddbbb0d0eb2ab8d276fcc5110a9d8ff
Ruben Burgos <code.rburgos@gmail.com>
2022-10-19 14:20:24 -0500

    Selection_Statements Finished

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Try to guess my favorite number between 1 and 1000!!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low, better luck next time!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high, better luck next time!");
+            }
+            else
+            {
+                Console.WriteLine($"Correct! My favorite number is {favNumber}!");
+            }
+
+
         }
     }
 }
